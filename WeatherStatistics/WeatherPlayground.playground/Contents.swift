feb543c66bdd0afe15bd0f24521e018464217db1
Nikola Andriiev <ahdpeeb.h@gmail.com>
2017-08-24 13:59:26 +0300

    commit 1

--- WeatherStatistics/WeatherPlayground.playground/Contents.swift
+++ WeatherStatistics/WeatherPlayground.playground/Contents.swift
@@ -10,31 +10,5 @@ import RxCocoa
 
 let diposeBag = DisposeBag()
 
-//let testCity = CitySourceData(name: "Bradford", url: "http://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/bradforddata.txt")
-
-//DataParser.loadWeather(city: testCity)
-//    .asObservable()
-//    .subscribe(onNext: { resnponce in
-//        print(resnponce)
-//    }, onError: { (error) in
-//        print(error)
-//    }).addDisposableTo(diposeBag)
-//
-//data(.get, "http://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/bradforddata.txt")
-//    .asObservable()
-//    .map({ (date) -> String in
-//        return String(bytes: date, encoding: .utf8)!
-//    })
-//______________________________________________________________________________________
-
-//let locationString = "Location 414900E 435200N, Lat 53.813 Lon -1.772, 134 metres amsl\n"
-//
-//let components = locationString.components(separatedBy: ",")
-//let locationComponents = components[1].components(separatedBy: " ")
-//
-//var altitude = components[2].components(separatedBy: " ")[1]
-//
-//let latitude = locationComponents[2]
-//let longiture = locationComponents[4]
-
-//__________________________________________________________-
+// test commit one 
+print("commit")
